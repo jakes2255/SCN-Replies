79ab9af4b668cdbef4151f1445612491a166917b
Arun Jacob <40160913+jakes2255@users.noreply.github.com>
2025-04-14 09:12:16 +0530

    Update simpleArrayOps.js
    toSpliced() method included

--- simpleArrayOps.js
+++ simpleArrayOps.js
@@ -7,3 +7,20 @@ console.log(arr); // [3, 1, 4, 1, 5]
 // Immutable reversing
 console.log(arr.toReversed()); // [5, 1, 4, 1, 3]
 console.log(arr); // [3, 1, 4, 1, 5]
+
+const months = ["Jan", "Mar", "Apr", "May"];
+
+// Inserting an element at index 1
+const months2 = months.toSpliced(1, 0, "Feb");
+console.log(months2); // ["Jan", "Feb", "Mar", "Apr", "May"]
+
+// Deleting two elements starting from index 2
+const months3 = months2.toSpliced(2, 2);
+console.log(months3); // ["Jan", "Feb", "May"]
+
+// Replacing one element at index 1 with two new elements
+const months4 = months3.toSpliced(1, 1, "Feb", "Mar");
+console.log(months4); // ["Jan", "Feb", "Mar", "May"]
+
+// Original array is not modified
+console.log(months); // ["Jan", "Mar", "Apr", "May"]
